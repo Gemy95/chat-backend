b93778d53ffa9763c9f45aab1b7f24466c79d98e
Ali Gamal <ali.gamal@beyond-solution.com>
2022-11-26 19:14:32 +0200

    NF: add relatime lib

--- micro-services/chat-service/src/app.module.ts
+++ micro-services/chat-service/src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
+import { REDIS_USER_NAME_SPACE } from './common/constants';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return {
+          readyLog: true,
+          config: [
+            {
+              url: configService.get<string>('REDIS_URL'),
+              namespace: REDIS_USER_NAME_SPACE,
+              onClientCreated(client) {
+                client.on('error', (err) => {
+                  console.log(err);
+                });
+              },
+            },
+          ],
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
