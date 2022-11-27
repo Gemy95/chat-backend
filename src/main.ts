9fed770557989bbb790aa1c164776d4a4c524da1
Ali Gamal <ali.gamal@beyond-solution.com>
2022-11-27 23:23:15 +0200

    NF: update main service

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './interceptors/error-handlling.interceptor';
 import { TransformInterceptor } from './interceptors/response-handlling.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
+  app.use(json({ limit: '5mb' }));
+  app.use(urlencoded({ extended: true }));
 
   const config = new DocumentBuilder()
     .setTitle('Chat example')
